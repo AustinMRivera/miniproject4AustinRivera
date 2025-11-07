9fdf8266af247993639f0154f0a817d24c5e0b2b
AustinMRivera <amrivera5@mail.fhsu.edu>
2025-11-06 21:04:07 -0600

    Add CatalogAppConfig for catalog_app

---
+++ catalog_app/apps.py
@@ -0,0 +1,9 @@
+# INF601 - Advanced Programming in Python
+# Austin Rivera
+# Mini Project 4
+
+from django.apps import AppConfig
+
+class CatalogAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'catalog_app'
